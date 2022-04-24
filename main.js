fce83826ba81a2e77e159dc7f4c54bbbb3fe06b0
Gabriel Santana Da Silva <gsantana@acesso.com>
2022-04-24 11:36:32 -0300

    [init]: initialize repo

---
+++ main.js
@@ -0,0 +1,11 @@
+import './styles/reset.css';
+import './styles/base.css';
+import './styles/header.css';
+import './styles/menu.css';
+import './styles/main.css';
+import './styles/footer.css';
+import './styles/card.css';
+
+document.querySelector('.menu__hambug').addEventListener('change', (e) => {
+  console.log(e);
+});
